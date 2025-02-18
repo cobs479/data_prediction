ab79b96641df1de54293461cb6caecd36e58c005
cobs479 <christineong974@gmail.com>
2025-02-18 22:30:04 +0800

    Update model_random_forest.py

--- model_random_forest.py
+++ model_random_forest.py
@@ -200,6 +200,9 @@ def predict_random_forest(field, start_date, end_date, location_select):
 
     X_predict.fillna(0, inplace=True)
 
+    st.dataframe(X_valid)
+    st.dataframe(X_predict)
+
     X_train_full, X_valid_full, y_train, y_valid = train_test_split(
         X, y, train_size=0.8, test_size=0.2, random_state=0)
 
@@ -280,9 +283,6 @@ def predict_random_forest(field, start_date, end_date, location_select):
 
     rmse_score = np.sqrt(mse_score)
     print('RMSE:', rmse_score)
-
-    st.dataframe(X_valid)
-    st.dataframe(X_predict)
 
     display_graph(X_predict, preds, start_date, end_date, location_select)
     display_table(X_predict, preds, start_date, end_date, location_select)
