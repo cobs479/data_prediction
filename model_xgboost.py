bac276876d8ffd992a37a531d7fc0b531f83d447
cobs479 <christineong974@gmail.com>
2025-02-19 08:29:32 +0800

    Update model_xgboost.py

--- model_xgboost.py
+++ model_xgboost.py
@@ -255,6 +255,8 @@ def predict_xgboost(field, start_date, end_date, location_select):
     X_valid = pipeline.transform(X_valid)
     X_predict = pipeline.transform(X_predict)
 
+    X_predict = pd.DataFrame(X_predict, columns=cols)
+
     if os.path.exists(model_save_path):
         print("Loading model from previous save...")
         model = joblib.load(model_save_path)
